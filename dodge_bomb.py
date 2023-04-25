2a491170bf6c44557d57a2cd7f47bf49f0a12b69
5Kis2STRONG <c0a22114d7@edu.teu.ac.jp>
2023-04-25 17:41:16 +0900

    最終

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -6,7 +6,7 @@ houkou = {
         pg.K_UP: (0, -1),
         pg.K_DOWN: (0, +1),
         pg.K_LEFT: (-1, 0),
-        pg.K_RIGHT: (+1, 0),,
+        pg.K_RIGHT: (+1, 0),
         }
 
 def check_bound(scr_rct: pg.Rect, obj_rct: pg.Rect) -> tuple[bool, bool]:
@@ -28,8 +28,8 @@ def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((1600, 900))
     clock = pg.time.Clock()
-    bg_img = pg.image.load("fig/pg_bg.jpg")
-    kk_img = pg.image.load("fig/3.png")
+    bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
+    kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
     bb_img = pg.Surface((20, 20))
     pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)  
@@ -99,7 +99,7 @@ def main():
         if not tate:
             vy *= -1
         if kk_rct.colliderect(bb_rct): 
-            kk_img = pg.image.load("fig/7.png")
+            kk_img = pg.image.load("ex02/fig/7.png")
             kk_img = pg.transform.rotozoom(kk_img, 0, 10)
             overtime = tmr
             gameover = True
