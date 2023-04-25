2f8d2f581e89536fae74ad15b69df71e0dcf36e0
5Kis2STRONG <c0a22114d7@edu.teu.ac.jp>
2023-04-25 14:52:27 +0900

    練習4

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -3,23 +3,30 @@ import sys
 import random as rd
 
 def main():
+
+    houkou = {
+        pg.K_UP: (0, -1),
+        pg.K_DOWN: (0, +1),
+        pg.K_LEFT: (-1, 0),
+        pg.K_RIGHT: (+1, 0)
+            }
+
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((1600, 900))
     clock = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
     kk_img = pg.image.load("fig/3.png")
-    bomb_img = pg.Surface((20, 20))
-    pg.draw.circle(bomb_img, (255, 0, 0), (10, 10), 10)
-    bomb_img.set_colorkey((0,0,0))
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
-    key_lst = pg.key.get_pressed()
-    if key_lst[pg.K_UP]:
-        kk_img.move_ip((0, -1))
-    bomb_X = rd.randint(0, 1600)
-    bomb_Y = rd.randint(0, 900)
+    bb_img = pg.Surface((20, 20))
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)  
+    bb_img.set_colorkey((0, 0, 0))  
+    x, y = rd.randint(0, 1600), rd.randint(0, 900)  
+    vx, vy = +1, +1  
+    bb_rct = bb_img.get_rect()
+    bb_rct.center = x, y   
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = 900, 400
     tmr = 0
-    vx = 0
-    vy = 0
 
     while True:
         for event in pg.event.get():
@@ -27,14 +34,18 @@ def main():
                 return 0
 
         tmr += 1
-        vx += 1
-        vy += 1
+
+        key_lst = pg.key.get_pressed()
+        for k, mv in houkou.items():
+            if key_lst[k]:
+                kk_rct.move_ip(mv)
+
         screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, [900, 400])
-        screen.blit(bomb_img, [bomb_X + vx, bomb_Y + vy])
+        screen.blit(kk_img, kk_rct)
+        bb_rct.move_ip(vx, vy)  
+        screen.blit(bb_img, bb_rct) 
         pg.display.update()
         clock.tick(1000)
-
 
 if __name__ == "__main__":
     pg.init()
